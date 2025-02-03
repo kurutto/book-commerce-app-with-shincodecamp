29f4b54089d0029ff78baf151f2f73177d9f0ea5
kurutto <namihojo@gmail.com>
2025-02-03 22:25:42 +0900

    vercelエラー修正： src/app/api/purchases/[userId]/route.ts

--- src/app/api/purchases/[userId]/route.ts
+++ src/app/api/purchases/[userId]/route.ts
@@ -1,7 +1,7 @@
 import prisma from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
 //購入履歴検索API
-export async function GET(request:Request,{params}:{params:{userId:string}}){
+export async function GET({params}:{params:{userId:string}}){
   const userId = params.userId;
   try{
     const purchases = await prisma.purchase.findMany({
