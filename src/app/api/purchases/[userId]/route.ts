d143121041891114e443df4d39bf66a6261c737c
kurutto <namihojo@gmail.com>
2025-02-04 16:23:55 +0900

    paramsのエラー

--- src/app/api/purchases/[userId]/route.ts
+++ src/app/api/purchases/[userId]/route.ts
@@ -3,9 +3,9 @@ import { NextResponse } from "next/server";
 //購入履歴検索API
 export async function GET(
   request: Request,
-  { params }: { params: { userId: string } }
+  context:{ params: { userId: string } }
 ) {
-  const { userId } = params;
+  const { userId } = context.params;
 
   try {
     const purchases = await prisma.purchase.findMany({
