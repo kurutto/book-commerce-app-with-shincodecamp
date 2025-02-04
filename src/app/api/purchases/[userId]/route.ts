76e26f3cbf4d61cf10bf0c98d176f58e221f905a
kurutto <namihojo@gmail.com>
2025-02-04 17:06:36 +0900

    userId取得のエラーを修正" " ç 	&

--- src/app/api/purchases/[userId]/route.ts
+++ src/app/api/purchases/[userId]/route.ts
@@ -1,11 +1,8 @@
 import prisma from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
 //購入履歴検索API
-export async function GET(
-  request: Request,
-  context: { params: { userId: string } }
-) {
-  const { userId } = context.params;
+export async function GET(request: Request) {
+  const userId = request.url.split("purchases/")[1];
   try {
     const purchases = await prisma.purchase.findMany({
       where: {
