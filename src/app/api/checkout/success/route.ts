ef1f8f03f94d4b582c88d13ca5997e93ed19c48a
kurutto <namihojo@gmail.com>
2025-02-04 10:02:34 +0900

    Error: Neither apiKey nor config.authenticator provided

--- src/app/api/checkout/success/route.ts
+++ src/app/api/checkout/success/route.ts
@@ -2,10 +2,9 @@ import prisma from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
-
 //購入履歴の保存
 export async function POST(request: Request) {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
   //sessionIdはbook/checkout-success（購入完了画面）を表示した時にurlにつけられるid
   const { sessionId } = await request.json();
   try {
